b9115a531e6e8f8c1a36e608529706c71b0cdab9
braxb00 <111767117+braxb00@users.noreply.github.com>
2022-11-28 10:58:38 -0600

    final-upload
    final upload with scrolling functionality and minor tweaks

---
+++ script/javascript.js
@@ -0,0 +1,19 @@
+$(".button2").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".history").offset().top
+    }, 500);});
+
+$(".button3").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".shop").offset().top
+    }, 500);});
+
+$(".button4").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".contact-us").offset().top
+    }, 500);});
+
+$(".return").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".title").offset().top
+    }, 500);});
